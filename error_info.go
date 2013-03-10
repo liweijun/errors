d6eab34291c0cd7bf78e26f7ba49de08421c813e
xushiwei <xushiweizh@gmail.com>
2013-03-10 19:52:06 +0800

    error info

---
+++ error_info.go
@@ -0,0 +1,89 @@
+package errors
+
+import (
+	"fmt"
+	"github.com/qiniu/log"
+)
+
+const prefix = " ==> "
+
+// --------------------------------------------------------------------
+
+type errorDetailer interface {
+    ErrorDetail() string
+}
+
+func Detail(err error) string {
+	if e, ok := err.(errorDetailer); ok {
+		return e.ErrorDetail()
+	}
+	return prefix + err.Error()
+}
+
+// --------------------------------------------------------------------
+
+type ErrorInfo struct {
+	Err error
+	Why error
+	Cmd []interface{}
+}
+
+func Info(err error, cmd ...interface{}) *ErrorInfo {
+	if e, ok := err.(*ErrorInfo); ok {
+		err = e.Err
+	}
+	return &ErrorInfo{Cmd: cmd, Err: err}
+}
+
+func (r *ErrorInfo) Error() string {
+	return r.Err.Error()
+}
+
+func (r *ErrorInfo) ErrorDetail() string {
+	e := prefix + r.Err.Error() + " ~ " + fmt.Sprintln(r.Cmd...)
+	if r.Why != nil {
+		e += Detail(r.Why)
+	}
+	return e
+}
+
+func (r *ErrorInfo) Detail(err error) *ErrorInfo {
+	r.Why = err
+	return r
+}
+
+func (r *ErrorInfo) Method() (cmd string, ok bool) {
+	if len(r.Cmd) > 0 {
+		cmd, ok = r.Cmd[0].(string)
+	}
+	return
+}
+
+func (r *ErrorInfo) LogMessage() string {
+	detail := r.ErrorDetail()
+	if cmd, ok := r.Method(); ok {
+		detail = cmd + " failed:\n" + detail
+	}
+	return detail
+}
+
+func (r *ErrorInfo) Warn() *ErrorInfo {
+	log.Std.Output("", log.Lwarn, 2, r.LogMessage())
+	return r
+}
+
+// --------------------------------------------------------------------
+
+func Err(err error) error {
+	for {
+		if e, ok := err.(*ErrorInfo); ok {
+			err = e.Err
+		} else {
+			break
+		}
+	}
+	return err
+}
+
+// --------------------------------------------------------------------
+
